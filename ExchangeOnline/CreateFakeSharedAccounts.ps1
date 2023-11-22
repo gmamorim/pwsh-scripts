0cf9848406329eac7d3399cdb858248a157fe3a9
Gabriel Amorim <gabriel.amorim@live.com>
2023-11-22 11:59:49 +0100

    modified:   ExchangeOnline/CreateFakeSharedAccounts.ps1

--- ExchangeOnline/CreateFakeSharedAccounts.ps1
+++ ExchangeOnline/CreateFakeSharedAccounts.ps1
@@ -1,10 +1,13 @@
-# Function to generate a random string
+# Function to generate a random string for the local part of the email
 function Get-RandomString {
     param(
         [int]$length = 10
     )
     return -join ((65..90) + (97..122) | Get-Random -Count $length | % {[char]$_})
 }
+
+# Prompt for the domain to be used
+$domain = Read-Host "Enter the domain to be used for mailboxes"
 
 # Prompt for the number of fake mailboxes to create
 $numberOfMailboxes = Read-Host "Enter the number of fake shared mailboxes to create"
@@ -13,10 +16,10 @@ $numberOfMailboxes = Read-Host "Enter the number of fake shared mailboxes to cre
 for ($i = 1; $i -le $numberOfMailboxes; $i++) {
     $mailboxName = "FakeMailbox" + $i
     $alias = "fakemailbox" + $i
-    $userPrincipalName = $alias + "@" + (Get-RandomString -length 5) + ".com"
+    $emailAddress = $alias + "@" + $domain
 
     # Command to create the shared mailbox
-    New-Mailbox -Name $mailboxName -Alias $alias -Shared -UserPrincipalName $userPrincipalName
+    New-Mailbox -Name $mailboxName -Alias $alias -Shared -PrimarySmtpAddress $emailAddress
 
-    Write-Host "Created shared mailbox: $mailboxName with UPN: $userPrincipalName"
+    Write-Host "Created shared mailbox: $mailboxName with email: $emailAddress"
 }
